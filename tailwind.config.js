590bd979811efac5449a086a3be6386e5e76c5fc
rakafebriansy <kerenr445@gmail.com>
2024-04-20 21:15:11 +0700

    feat: navbar, hero, features

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'igniter': '#CC532A'
+      }
+    },
+    fontFamily: {
+      ralewayExtraLight: ['"Raleway ExtraLight"'],
+      ralewayLight: ['"Raleway Light"'],
+      ralewayRegular: ['"Raleway Regular"'],
+      ralewayMedium: ['"Raleway Medium"'],
+      ralewaySemiBold: ['"Raleway SemiBold"'],
+      ralewayBold: ['"Raleway Bold"'],
+    },
   },
   plugins: [],
 }
